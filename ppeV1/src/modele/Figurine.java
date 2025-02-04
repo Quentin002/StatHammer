6118464616ba85178e0ea08214b114c9851c597f
Quentin002 <quentinmartins1@gmail.com>
2025-02-04 13:56:37 +0100

    voila

---
+++ ppeV1/src/modele/Figurine.java
@@ -0,0 +1,46 @@
+package modele;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Figurine {
+	ArrayList<Arme> armes;
+	ArrayList<Aptitude> aptidudes;
+	String nom;
+	String logoChemin;
+	int M;
+	int E;
+	int SV;
+	int HP;
+	int CD;
+	int CO;
+	public Figurine(ArrayList<Arme> armes, ArrayList<Aptitude> aptidudes, String nom, String logoChemin, int m, int e, int sV, int hP, int cD, int cO) {
+		super();
+		this.armes = armes;
+		this.aptidudes = aptidudes;
+		this.nom = nom;
+		this.logoChemin = logoChemin;
+		M = m;
+		E = e;
+		SV = sV;
+		HP = hP;
+		CD = cD;
+		CO = cO;
+	}
+	
+	//renvoi un dico avec en clé les noms des stats des armes et en valeurs une liste de valeur de toute les armes 
+	public HashMap<String,Arme> statAtk(){
+		HashMap<String,Arme> liste = new HashMap<String, Arme>();
+		//Ajouter du code a prendre depuis arme
+		return liste;
+	}
+	public void updateHP(int dommage) {
+		this.HP +=dommage;
+	}
+	public ArrayList<Arme> defense(){
+		ArrayList<Arme> liste = new ArrayList<Arme>();
+		return liste;
+	}
+	
+}
+
