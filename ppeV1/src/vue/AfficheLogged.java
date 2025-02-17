fc5afdf952ba3dbc419acf6b7f9fbac27b6199d4
Quentin002 <quentinmartins1@gmail.com>
2025-02-17 13:17:31 +0100

    t

--- ppeV1/src/vue/AfficheLogged.java
+++ ppeV1/src/vue/AfficheLogged.java
@@ -2,23 +2,32 @@ package vue;
 
 
 
-import controlleur.BDD;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class AfficheLogged {
 	public static void affiche(Stage primaryStage) {
 		VBox root = new VBox();
 		Scene scene = new Scene(root,1200,800);
-		Rectangle rect = new Rectangle(50,50,Color.BLUE);
 		
+		Label titre = new Label("StatHammer");
+		VBox cadre = new VBox();
+		Button simulation = new Button("SIMULATION");
+		Button creeListe = new Button("Creer Liste");
+		Button gererListe = new Button("Gerer Liste");
 		
-		root.setAlignment(Pos.CENTER);
-		root.getChildren().add(rect);
+		root.getChildren().add(titre);
+		root.getChildren().add(cadre);
+		cadre.setAlignment(Pos.CENTER_RIGHT);
+		cadre.getChildren().add(simulation);
+		cadre.getChildren().add(creeListe);
+		cadre.getChildren().add(gererListe);
+		root.setAlignment(Pos.TOP_CENTER);
+		
 		
 		primaryStage.setScene(scene);
 		primaryStage.show();
