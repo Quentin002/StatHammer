66c9c841f0acbe0ffee8ebcfc6e9a9a146ee3615
YannBlev <36kbrl@gmail.com>
2025-04-22 08:37:15 +0200

    maj fichier js

--- ppeWeb/src/main/webapp/js/evtMontrerNom.js
+++ ppeWeb/src/main/webapp/js/evtMontrerNom.js
@@ -2,16 +2,10 @@ document.getElementById('imageUpload').addEventListener('change', function() {
     const label = this.nextElementSibling;
     const fileName = this.files[0] ? this.files[0].name : "Choisir une image";
     label.innerText = fileName;
-});
-
-document.getElementById('imageUpload').addEventListener('change', function() {
-    const label = this.nextElementSibling;
-    const fileName = this.files[0] ? this.files[0].name : "Choisir une image";
-    label.innerText = fileName;
-
-    if (this.files.length > 0) {
-        this.parentElement.classList.add('filled');
-    } else {
-        this.parentElement.classList.remove('filled');
-    }
+	
+	if (this.files.length > 0) {
+	    this.parentElement.classList.add('filled');
+	} else {
+	    this.parentElement.classList.remove('filled');
+	}
 });
