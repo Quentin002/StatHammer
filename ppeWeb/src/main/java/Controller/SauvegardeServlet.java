b88da0df851ee50100eba7a2eb91cd3aaca00bd7
Quentin002 <quentinmartins1@gmail.com>
2025-04-28 10:00:47 +0200

    BANDE DE FLEMMARD DANS ARMEELISTE

--- ppeWeb/src/main/java/Controller/SauvegardeServlet.java
+++ ppeWeb/src/main/java/Controller/SauvegardeServlet.java
@@ -8,7 +8,11 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
+import Model.ArmeeListe;
+import Model.Unit;
 import Model.User;
 
 /**
@@ -32,12 +36,23 @@ public class SauvegardeServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		HttpSession session = request.getSession();
+		ArrayList<ArmeeListe> userList = (ArrayList<ArmeeListe>) session.getAttribute("listes");
+		if(userList==null) {
+			userList = new ArrayList<ArmeeListe>();
+		}
 		StockageCreerListe.getArmeeListe().setNom((String) session.getAttribute("nomListe"));
 		String nom = (String) session.getAttribute("nom");
 		int id = (int) session.getAttribute("id");
 		String role = (String) session.getAttribute("role");
 		Instanciation.insertListe(StockageCreerListe.getArmeeListe(), new User(nom,id,role));
-		response.sendRedirect("AccueilView");
+		ArrayList<String> nomListe = new ArrayList<String>();
+		for(Unit unit:StockageCreerListe.getArmeeListe().getUnits()) {
+			nomListe.add(unit.getName());
+		}
+		StockageCreerListe.getArmeeListe().setUniteListe(nomListe);
+		userList.add(StockageCreerListe.getArmeeListe());
+		session.setAttribute("listes", userList);
+		response.sendRedirect("accueil");
 	}
 
 	/**
