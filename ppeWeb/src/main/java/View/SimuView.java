f97c8995019db726126bd418fad36b6fe56741c9
Quentin002 <quentinmartins1@gmail.com>
2025-05-05 15:50:45 +0200

    maj

--- ppeWeb/src/main/java/View/SimuView.java
+++ ppeWeb/src/main/java/View/SimuView.java
@@ -54,7 +54,8 @@ public class SimuView extends HttpServlet {
 	    String header = ConnexionView.headerTop + titre + header_bottom;
 	    out.println(header + AccueilView.barDeNav);
 	    
-	    ArrayList<Model.ArmeeListe> listes = (ArrayList<Model.ArmeeListe>) session.getAttribute("listes");
+	    @SuppressWarnings("unchecked")
+		ArrayList<Model.ArmeeListe> listes = (ArrayList<Model.ArmeeListe>) session.getAttribute("listes");
 	    request.setAttribute("listes", listes); // => simulation.jsp
 	    
 	    RequestDispatcher dispatcher = request.getRequestDispatcher("simulation.jsp");
